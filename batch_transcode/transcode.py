58ebe8222c50e38664668f9d8434d7b80e9fa43a
dlasley <none@none>
2013-06-01 02:16:24 +0000

    Added doc strings, cleaned code up
    --HG--
    extra : convert_revision : svn%3A50a07768-49b9-434d-829c-127858bbdbd1/trunk%4027

--- batch_transcode/transcode.py
+++ batch_transcode/transcode.py
@@ -17,6 +17,10 @@ import logging
 import math
 import hashlib
 
+logging.basicConfig(format='%(asctime)s %(message)s', level=logging.DEBUG)
+
+#   OS flags...
+#   @todo - Fix Winblows
 if os.name == 'nt':
     import win32api,win32process,win32con
     NICE_LVL = 0
@@ -37,19 +41,17 @@ else:
     DEV_NULL = u'/dev/null'
     WINDOWS = False
 
-logging.basicConfig(format='%(asctime)s %(message)s', level=logging.DEBUG)
-
 class transcode(object):
-    dry_runs        =   {
+    DRY_RUNS        =   {
         'demux'     :   False,
         'transcode' :   False,
         'remux'     :   False,
         'dont_delete':  False,
     } 
-    dir_permissions =   0777
-    track_type_order=   ('Video','Audio','Text')
-    settings_file = 'video_settings.xml'
-    transcode_settings = {
+    DIR_PERMISSIONS =   0777
+    TRACK_TYPE_ORDER=   'Video', 'Audio', 'Text'
+    SETTINGS_FILE = 'video_settings.xml'
+    TRANSCODE_SETTINGS = {
         'x264'      :   {
                             
                         },
@@ -65,20 +67,20 @@ class transcode(object):
         'fix_dvds'  :   False,
         'deinterlace':  False,
         'container' :   'mkv',
-        'br_percent':   False   #   Percent of bitrate (INT), False is go with program defaults
+        'br_percent':   False,   #   Percent of bitrate (INT), False is go with program defaults
     }
     #   Base 10 on purpose!
-    to_bit_map      =   {
+    TO_BIT_MAP      =   {
         'Gbps'  :   1000000000,#1073741824,
         'Mbps'  :   1000000,#1048576,
         'Kbps'  :   1000,#1024,
-        'bps'   :   1
+        'bps'   :   1,
     }
-    file_extension_map = {
+    FILE_EXTENSION_MAP = {
         'failsafes' :   {
             'Video' :   'avc',
             'Audio' :   'ac3',
-            'Text'  :   'sup'
+            'Text'  :   'sup',
         },
         'regexes':{
             'MPEG'  :   'mpg',
@@ -88,10 +90,16 @@ class transcode(object):
             'PCM'   :   'pcm',
         }
     }
-    vid_exts        =   ['.mkv','.m4v','.mp4','.mpg','.avi']
-    native_language =   ('English','eng')
+    VID_EXTS        =   ['.mkv', '.m4v', '.mp4', '.mpg', '.avi', ]
+    NATIVE_LANGUAGE =   'English', 'eng'
     THREADS = 1
-    def __init__(self,out_dir,debug=False):
+    def __init__(self, out_dir, debug=False, ):
+        '''
+            Initiate object..only necessary for directory encoding
+            
+            @param  out_dir str     Save directory
+            @param  debug   bool    Debugging?
+        '''
         self.out_dir = out_dir
         #   Generate the dirs
         self.encode_dir      =   os.path.join(out_dir,'encodeBox')
@@ -100,17 +108,15 @@ class transcode(object):
         self.log_file        =   os.path.join(self.error_dir,'error_log.log')
         for i in (self.encode_dir,self.finished_dir,self.error_dir):
             if not os.path.isdir(out_dir):
-                os.mkdir(i, self.dir_permissions)
-
+                os.mkdir(i, self.DIR_PERMISSIONS)
         self.__debug = debug
         self.cleanup_files = []
 
-    def encode_directory(self,inpath):
+    def encode_directory(self, inpath, ):
         '''
             Encodes an entire directory
             
             @param  String  inpath  Directory to encode
-            
             @return List    Newly created files
         '''
         self.new_files = []
@@ -118,7 +124,7 @@ class transcode(object):
         def thread(root,new_root,file_name,extension,transcode_settings):
             self.new_files.append( self.encode_it(os.path.join(root,'%s%s'%(file_name,extension)), os.path.join(new_root, '%s%s'%(file_name,extension)),transcode_settings) )
             del self.worker_threads[file_name]
-            if True not in self.dry_runs:   #<  Delete the file if not testing or specified
+            if True not in self.DRY_RUNS:   #<  Delete the file if not testing or specified
                 os.unlink(os.path.join(root,file_name,extension))
                 logging.debug('DELETED: %s'%os.path.join(root,file_name,extension))
         for root, dirs, files in os.walk(inpath):
@@ -129,15 +135,15 @@ class transcode(object):
                 if not os.path.isdir(new_root):
                     os.mkdir(new_root)
                 try:
-                    with open(os.path.join(root,self.settings_file)) as f: pass
-                    transcode_settings = self.parse_video_settings(os.path.join(root,self.settings_file))
+                    with open(os.path.join(root,self.SETTINGS_FILE)) as f: pass
+                    transcode_settings = self.parse_video_settings(os.path.join(root,self.SETTINGS_FILE))
                     print transcode_settings
                 except IOError:
                     transcode_settings = {}
                 #exit()
                 for file_name in files:
                     file_name,extension = os.path.splitext(file_name)
-                    if extension in self.vid_exts:
+                    if extension in self.VID_EXTS:
                         #print len(worker_threads)
                         while len(self.worker_threads) >= self.THREADS:
                             time.sleep(10)  #<   Wait a bit
@@ -150,19 +156,20 @@ class transcode(object):
                         #print new_files
                         #exit()
         #print new_files
-    def encode_it(self,file_path,new_file,transcode_settings={}):
+    def encode_it(self,file_path, new_file, transcode_settings={}, ):
         '''
             Encode wrapper, whole process
             
-            @param  String  file_path   In file
-            
-            @return String  Out file
+            @param  file_path           str     In file
+            @param  new_file            str     New file path
+            @param  transcode_settings  dict    Override defaults
+            @return str     Out file
         '''
         try:
             media_info = self.media_info(file_path)
             cleanup_files = []
             lng_codes = transcode.lng_codes()
-            demuxed = self.demux(file_path,media_info,self.encode_dir,dry_run=self.dry_runs['demux'])
+            demuxed = self.demux(file_path,media_info,self.encode_dir,dry_run=self.DRY_RUNS['demux'])
             cleanup_files.extend(demuxed)
             duped = self.compare_tracks(demuxed)
             #media_info['tracks'][i+1]   :   mux_files[i]
@@ -172,16 +179,17 @@ class transcode(object):
                     os.path.join(
                         self.encode_dir,
                         u'%s.%s'%(os.path.basename(file_path),
-                                  self.transcode_settings['container'])
+                                  self.TRANSCODE_SETTINGS['container'])
                     ),
                     media_info['tracks'][vid_id],
-                    dry_run=self.dry_runs['transcode']
+                    transcode_settings,
+                    dry_run=self.DRY_RUNS['transcode']
                 )
                 cleanup_files.append(demuxed[vid_id-1])
                 logging.debug('Transcoded %s' % demuxed[vid_id-1])
             track_order = self.choose_track_order(media_info)
             logging.debug('Track Order Chosen!')
-            new_file = transcode.remux(demuxed,media_info,new_file,duped,track_order,dry_run=self.dry_runs['remux'])
+            new_file = transcode.remux(demuxed,media_info,new_file,duped,track_order,dry_run=self.DRY_RUNS['remux'])
             logging.debug('New File Success %s' % new_file)
             
         except Exception as e: #< Trigger error, leave file in place
@@ -203,8 +211,8 @@ class transcode(object):
     '''     Static Methods      '''
     @staticmethod
     def lng_codes():
-        ##  Returns Available Language Codes    @deprecated
-        #   @return Dict    Language Codes, { short_code : long_code }
+        '''  Returns Available Language Codes    @deprecated
+            @return Dict    Language Codes, { short_code : long_code } '''
         mkvmerge_lng_codes = subprocess.check_output([MKVMERGE_PATH, '--list-languages'])
         lng_codes = {}
         for row in mkvmerge_lng_codes.split('\n'):
@@ -217,12 +225,15 @@ class transcode(object):
         return lng_codes
     
     @staticmethod
-    def command_with_priority(command,shell=False,cwd='./'):
-        ##  Runs a command using subprocess. Sets the priority.
-        #   @param  List    command Command to execute
-        #   @param  Bool    shell   Use shell
-        #   @param  Str     cwd     Current working directory
-        #   @return Tuple   (returncode, stdoutdata, stderrdata)
+    def command_with_priority(command, shell=False, cwd='./', ):
+        '''
+            Runs a command using subprocess. Sets the priority.
+            
+            @param  command str     Command to execute
+            @param  shell   bool    Use shell
+            @param  cwd     str     Current working directory
+            @return tuple   (returncode, stdoutdata, stderrdata)
+        '''
         if WINDOWS:
             process = subprocess.Popen(command,shell=shell,cwd=cwd,
                                     stdout=subprocess.PIPE,
@@ -252,10 +263,16 @@ class transcode(object):
         communicate_return = process.communicate()
         if process.returncode != 0:
             logging.error('Command returned an error!\n\n%s\n\n%s\n\n' % (str(command), str(communicate_return)))
-        return (process.returncode,communicate_return[0],communicate_return[1])
+        return process.returncode, communicate_return[0], communicate_return[1]
 
     @staticmethod
-    def md5_sum(file_path):
+    def md5_sum(file_path, ):
+        '''
+            MD5 sum of a file
+            
+            @param  file_path   str     file path
+            @return str File md5
+        '''
         fh = open(file_path, 'rb')
         m = hashlib.md5()
         while True:
@@ -265,7 +282,13 @@ class transcode(object):
             m.update(data)
         return m.hexdigest()
     @staticmethod
-    def parse_video_settings(file_path):
+    def parse_video_settings(file_path, ):
+        '''
+            Parse settings.xml
+            
+            @param  file_path   str     File path of XML
+            @return dict    Transcode settings
+        '''
         transcode_settings = {}
         dom = xml.dom.minidom.parse(file_path)
         for settings_group in dom.getElementsByTagName('settings_group'):
@@ -288,9 +311,12 @@ class transcode(object):
                                 transcode_settings[settings_group_name][setting_name] = True
         return transcode_settings
     @staticmethod
-    def compare_tracks(demuxed_files):
+    def compare_tracks(demuxed_files, ):
         '''
             Make sure the tracks aren't the same, if they are, toss out the highest numbered
+            
+            @param  demuxed_files   list    List of demuxed files
+            @return list    files in order
         '''
         duped,track_sizes = [],{}
         for file_path in demuxed_files:
@@ -312,20 +338,19 @@ class transcode(object):
                         duped.append(track)
         return duped
     @staticmethod
-    def choose_track_order(media_info):
+    def choose_track_order(media_info, ):
         '''
             Choose track order, default track should be the first occurrence of each type
             
-            @param  Dict    media_info  Media info as returned by self.media_info()
-            
-            @return List    List of tracks in order
+            @param  media_info  dict  Media info as returned by self.media_info()
+            @return list    List of tracks in order
         '''
         non_english,english,track_order = [],[],[]
-        for track_type in transcode.track_type_order:
+        for track_type in transcode.TRACK_TYPE_ORDER:
             try:
                 for track_id in media_info['id_maps'][track_type]:
                     try:
-                        if media_info['tracks'][track_id]['Language'] == transcode.native_language[0]:
+                        if media_info['tracks'][track_id]['Language'] == transcode.NATIVE_LANGUAGE[0]:
                             english.append(track_id)
                         else:
                             non_english.append(track_id)
@@ -342,13 +367,12 @@ class transcode(object):
         return track_order
     
     @staticmethod
-    def media_info(file_path):
+    def media_info(file_path, ):
         '''
             Return merged video info (mkvmerge+mediainfo apps)
             
-            @param  String  file_path
-            
-            @return Dict    Media Info
+            @param  file_path   str     file path
+            @return dict    Media Info
         '''
         mkvmerge    =   transcode.mkvmerge_identify(file_path)
         mediainfo   =   transcode.mediainfo(file_path)
@@ -371,13 +395,12 @@ class transcode(object):
         return mediainfo
     
     @staticmethod
-    def mkvmerge_identify(file_path):
+    def mkvmerge_identify(file_path, ):
         '''
             Return the MKV Merge related info. This will be used to merge into data from media_info
             
-            @param  String  file_path   file path
-            
-            @return Dict    {MKVMerge_TrackID}{number,uid,codec_id,..}
+            @param  file_path   str     file path
+            @return dict    {MKVMerge_TrackID}{number,uid,codec_id,..}
         '''
         info_regex = re.compile('Track ID ([0-9]{1,2}): (\w+) \((.+)\) \[(([\w/]+:[\w/\+]+ ?)*)\]')
         info = transcode.command_with_priority( [ MKVMERGE_PATH, '--identify-verbose', file_path ] )[1]
@@ -402,13 +425,12 @@ class transcode(object):
         return info_out
         
     @staticmethod
-    def mediainfo(file_path):
+    def mediainfo(file_path, ):
         '''
             Return info from mediainfo app
             
-            @param  String  file_path   file path
-            
-            @return Dict    {tracks:[],id_maps{type:[]}} multi dimensional awesomeness
+            @param  file_path   str     file path
+            @return dict    {tracks:[],id_maps{type:[]}} multi dimensional awesomeness
         '''
         dom = xml.dom.minidom.parseString(
             transcode.command_with_priority(
@@ -437,12 +459,12 @@ class transcode(object):
                         track_info['Bit_rate']
                     except KeyError:
                         track_info['Bit_rate'] = tracks[0]['Overall_bit_rate']
-                for regex,extension in transcode.file_extension_map['regexes'].iteritems():
+                for regex,extension in transcode.FILE_EXTENSION_MAP['regexes'].iteritems():
                     if re.search(regex,track_info['Codec_ID']) is not None:
                         track_info['extension'] = extension
                         break
                 else:
-                    track_info['extension'] = transcode.file_extension_map['failsafes'][track_type]
+                    track_info['extension'] = transcode.FILE_EXTENSION_MAP['failsafes'][track_type]
             except KeyError:
                 pass
             try:
@@ -459,12 +481,23 @@ class transcode(object):
         return {'tracks':tracks,'id_maps':track_types}
     
     @staticmethod
-    def transcode(old_file,new_file,media_info,new_settings={},dry_run=False):
+    def transcode(old_file, new_file, media_info,
+                  new_settings={}, dry_run=False, ):
+        '''
+            Transcode a video
+            
+            @param  old_file        str     Old file path
+            @param  new_file        str     New file path
+            @param  media_info      dict    Dict returned by transcode.media_info(old_file)
+            @param  new_settings    dict    Transcode settings to override defaults
+            @param  dry_run         bool    Testing?
+            @return str New file path
+        '''
         log_file = os.path.basename(old_file)+'.log'
         self.cleanup_files.append(log_file) #< Fix #21
         cmd = [FFMPEG_LOCATION, u'-i', '"'+old_file+'"', u'-passlogfile' , '"'+log_file+'"' ]
         win_cmd = [FFMPEG_LOCATION, u'-i', old_file, u'-passlogfile' , log_file ]
-        transcode_settings = transcode.transcode_settings
+        transcode_settings = transcode.TRANSCODE_SETTINGS
         height = int(media_info['Height'].replace(' pixels','').replace(' ',''))
         width = int(media_info['Width'].replace(' pixels','').replace(' ',''))
         for setting_type,setting_group in new_settings.iteritems():
@@ -475,7 +508,7 @@ class transcode(object):
         else:
             br_modifier = transcode_settings['br_percent']
         br = media_info['Bit_rate'].rsplit(' ',1)
-        br = float(br[0].replace(' ','')) * transcode.to_bit_map[br[1]]
+        br = float(br[0].replace(' ','')) * transcode.TO_BIT_MAP[br[1]]
         transcode_settings['avconv']['b:v'] = int(round(float(br)*(float(br_modifier)/100)))
         for setting_name,setting_value in transcode_settings['avconv'].iteritems():
             if setting_value:
@@ -533,19 +566,26 @@ class transcode(object):
                     )[0] == 0: #< 2nd pass success
                         return new_file
     @staticmethod
-    def fix_dvds_cmd(height,width):
+    def fix_dvds_cmd(height, width, ):
+        '''
+            Crops black sidebars off of DVDs that were incorrectly recognized by MakeMKV
+            @param  height  int Vid height
+            @param  width   int Vid width
+            @return str Command to input into ffmpeg for cropping
+        '''
         return '-s %sX%s -vf crop=%s:%s:0:%s' % (width,height,width,
                                                  height-(height*.25),
                                                  height*.125)
     
     @staticmethod
-    def demux(file_path, media_info, out_path, dry_run=False):
+    def demux(file_path, media_info, out_path, dry_run=False, ):
         '''
             Demux file
             
-            @param  String  file_path   File path
+            @param  Str     file_path   File path
             @param  Dict    media_info  Media info as returned by self.media_info()
-            
+            @param  Str     out_path    Save to path
+            @param  Bool    dry_run     Do not run command if True
             @return List    List of extracted tracks
         '''
         cmd = [MKVEXTRACT_PATH, u'tracks', file_path]
@@ -569,7 +609,8 @@ class transcode(object):
             if transcode.command_with_priority(cmd, cwd=out_path)[0] == 0:
                 return demuxed
     @staticmethod
-    def remux(mux_files,media_info,new_file,dups=[],track_order=False,dry_run=False):
+    def remux(mux_files, media_info, new_file, dups=[],
+              track_order=False, dry_run=False, ):
         '''
             Remux
             
@@ -579,7 +620,6 @@ class transcode(object):
             @param  List    dups        Output from transcode.compare_tracks()
             @param  List    track_order List of track order (transcode.choose_track_order())
             @param  Bool    dry_run     Testing? 
-            
             @return String  New file path
         '''
         logging.debug('In Remux')
